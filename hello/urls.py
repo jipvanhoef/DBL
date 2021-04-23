16110a1d70f2e277c257426692873e51a132063f
Jip van Hoef <jipvanhoef@gmail.com>
2021-04-23 14:50:08 +0200

    initial commit

---
+++ hello/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+]
+#yo yo yo I am in the file yes yes yes its an honour to be here
+#dont know what this file does exacly yet so dont touch please you cannot stop me
