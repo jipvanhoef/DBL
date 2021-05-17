c1e46e228473d13989c25061b9365eeb33d9610a
FriendedBreak <Thymofieten@gmail.com>
2021-05-17 14:56:09 +0200

    fixed css linking

--- web_project/settings.py
+++ web_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import mimetypes
 
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
