f9d475cbeefde75d18c1f5870e839673cb6c755c
FriendedBreak <Thymofieten@gmail.com>
2021-05-28 11:39:33 +0200

    added network graph

--- data_visualization/views.py
+++ data_visualization/views.py
@@ -7,12 +7,19 @@ from pathlib import Path
 from main.models import user_id, Data_set
 
 import os
-import datetime
-import sqlite3
-import threading
-
-import numpy as np #importing numpy 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import numpy as np #importing numpy and 
 import pandas as pd #importing pandas
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import timeit
+
+import datetime
+
 import plotly.graph_objects as go
 import plotly.io as pio
 
@@ -22,132 +29,83 @@ from plotly.graph_objs import *
 from random import randint
 
 import networkx as nx
+import numpy as np #importing numpy and 
+import pandas as pd #importing pandas
 
 
 import matplotlib as mpl
-import matplotlib.pyplot as plt, mpld3 #importing the mpld3 library to make graphs interactive
+import matplotlib.pyplot as plt
 import matplotlib.colors
 import seaborn as sns  # also improves the look of plots
-sns.set()  # set Seaborn defaults
-plt.rcParams['figure.figsize'] = [10, 5]  # default hor./vert. size of plots, in inches
-plt.rcParams['lines.markeredgewidth'] = 1  # to fix issue with seaborn box plots; needed after import seaborn
-
-
-# hide FutureWarnings, which may show for Seaborn calls in most recent Anaconda
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning) 
-
-line_graph = None
-network_graph = None
-
-#This function grabs the data set of the current user an creates the visualization
-def create_line_graph(path):
-
-    df_enron = pd.read_csv(path, parse_dates=['date']) #reading the enron csv and storing it as dataframe
-
-    df_enron_sorted = df_enron.sort_values(by='date') #making a sorted dataframe by sorting on the date
-    #df_enron_sorted.head() (disregard)
-
-    grouped = df_enron_sorted.groupby('date') #making a grouping object to group by date
-
-    df_average_sentiment = grouped[['sentiment']].mean() #making a new df where the average sentiment per day is stored
-
-    df_average_sentiment.head() 
-
-    fig = go.Figure() #defining a figure
-
-    fig.add_trace(
-        go.Scatter(x=list(df_average_sentiment.index), y=list(df_average_sentiment['sentiment']))) #adding the data to the figure
-
-    # Set title
-    fig.update_layout(
-        title_text="Average sentiment per day across all Enron emails (with interactive timeline)"
-    )
-
-    # Add range slider
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1,
-                         label="1m",
-                         step="month",
-                         stepmode="backward"),
-                    dict(count=6,
-                        label="6m",
-                        step="month",
-                        stepmode="backward"),
-                    dict(count=1,
-                        label="YTD",
-                        step="year",
-                        stepmode="todate"),
-                    dict(count=1,
-                         label="1y",
-                         step="year",
-                         stepmode="backward"),
-                    dict(step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-        type="date"
-        )
-    )
-    return fig
-    
 def create_network_graph(path):
-    
+    app = dash.Dash(__name__, )
+
+    styles = {
+        'pre': {
+            'border': 'thin lightgrey solid',
+            'overflowX': 'scroll'
+        }
+    }
+
+    sns.set()  # set Seaborn defaults
+    plt.rcParams['figure.figsize'] = [15, 20]  # default hor./vert. size of plots, in inches
+    plt.rcParams['lines.markeredgewidth'] = 1  # to fix issue with seaborn box plots; needed after import seaborn
 
     df_enron = pd.read_csv(path, parse_dates=['date']) #reading the enron csv and storing it as dataframe
+
     #get a list of all the years in the dataset
     df_enron['year'] = pd.DatetimeIndex(df_enron['date']).year
     years= list(df_enron['year'].unique())
     list.sort(years)
-
-    #make a new dataframe that counts all the mails that are sent from one person to another in one year
-    df_enron_test = df_enron.groupby(['year', 'fromId','toId', 'fromJobtitle', 'toJobtitle', 'fromEmail'])['toId'].nunique()
-    df =df_enron_test.to_frame()
+    #make a new dataframe that counts all the mails that are sent from one person to another per year year
+    df = df_enron.groupby(['year', 'fromId','toId', 'fromJobtitle', 'toJobtitle', 'fromEmail', 'toEmail' ]).agg({'toId':['count']})
     df.columns=['amount']
     df= df.reset_index()
     df=df.sort_values(by=['year','fromId'])
 
-    #make a new df from where all the nodes are made from
-    df_enron_to = df_enron.groupby('toId').agg({'sentiment':['mean'], 'toJobtitle': ['min'], 'fromEmail':['min']}).reset_index()
+    df_enron_sorted = df_enron.sort_values(by='date') #making a sorted dataframe by sorting on the date
+
+    df_time = df_enron_sorted.groupby(['date', 'fromEmail']).agg({'sentiment':['mean'], 'fromEmail': [ 'count']}).reset_index() #making a grouping object to group by date
+    df_time.columns =['date', 'fromEmail', 'sentiment', 'fromEmailCount']
+    #make a dataframe with all the from email adresses
+    df_enron_from = df_enron.groupby('fromId').agg({'sentiment':['mean'], 'fromJobtitle': ['min'], 'fromEmail':['min']}).reset_index()
+    df_enron_from.columns=['ID', 'mean sentiment', 'Jobtitle', 'fromEmail']
+    #make a dataframe with all the to email adresses
+    df_enron_to = df_enron.groupby('toId').agg({'sentiment':['mean'], 'toJobtitle': ['min'], 'toEmail':['min']}).reset_index()
     df_enron_to.columns=['ID', 'mean sentiment', 'Jobtitle', 'fromEmail']
+    #join to and from dataframes to make sure everyone is in the df ( if there is anyone that only sent or only received a mail they are still in here)
+    df_enron_tot = df_enron_from.combine_first(df_enron_to)
     
-    #define the senders and receivers as nodes
-    mailsenders = list(df["fromId"].unique()) 
+
+    mailsenders = list(df["fromId"].unique()) #define the senders and receivers as nodes
     mailreceivers = list(df["toId"].unique())
     jobtitles = list(df['fromJobtitle'].unique()) #list of all the job titles
+    jobtitlesindex = jobtitles.copy()
     node_list = list(set(mailsenders+mailreceivers)) #list of everything that needs to be a node
     G = nx.MultiGraph() #defining the graph
 
     jobtitlescount = len(jobtitles) #amount of functions in the company
     yearscount = len(years)#amount of years that mail are sent
     yearsstring = [str(i) for i in years]
-
     #make color lists for years and jobs
-    jobcolor = []
-    for i in range(jobtitlescount):
-        jobcolor.append('#%06X' % randint(0, 0xFFFFFF))
-    yearcolor = []
-    for i in range(yearscount):
-        jobcolor.append('#%06X' % randint(0, 0xFFFFFF))
-        
+    jobcolor = ['#377eb8', '#ff7f00', '#4daf4a',
+                '#f781bf', '#a65628', '#984ea3',
+                '#999999', '#e41a1c', '#dede00',
+                '#67001f', '#b2182b', '#d6604d',
+                '#f4a582', '#fddbc7', '#f7f7f7',
+                '#d1e5f0', '#92c5de', '#4393c3',
+                '#2166ac', '#053061']
+    
     for i in node_list:
-        G.add_node(i) #adding every node to the graph 
-        #G.nodes[i]['Title'] = df_enron_from['fromJobtitle'].loc[i]
-        
+        G.add_node(i) #adding every node to the graph     
     for i in mailreceivers:
-        G.nodes[i]['Title'] = df_enron_to['Jobtitle'].loc[i-1]
-        G.nodes[i]['mail'] = df_enron_to['fromEmail'].loc[i-1]
+        G.nodes[i]['Title'] = df_enron_tot['Jobtitle'].loc[i-1]
+        G.nodes[i]['mail'] = df_enron_tot['fromEmail'].loc[i-1]
+        G.nodes[i]['sentiment'] = df_enron_tot['mean sentiment'].loc[i-1]
 
-        G.nodes[i]['sentiment'] = df_enron_to['mean sentiment'].loc[i-1]
-        
     for i,j in df.iterrows(): #adding the edges
-        G.add_edges_from([(j["fromId"],j["toId"], {"year": df['year'].loc[i]})]) 
-        
+        G.add_edges_from([(j["fromId"],j["toId"], {"year": df['year'].loc[i]})])
+    
     #assigning a position to each node for plotting
     pos = nx.fruchterman_reingold_layout(G, k =1)
 
@@ -177,7 +135,7 @@ def create_network_graph(path):
                     hovermode='closest',
                     margin=dict(b=20,l=5,r=5,t=40),
                     annotations=[ dict(
-                    text="No. of connections",
+                    text="",
                     showarrow=False,
                     xref="paper", yref="paper") ],
                     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
@@ -204,21 +162,30 @@ def create_network_graph(path):
                 visible = True,
                 text=[],
                 name = jobtitles[i],
+                customdata = [],
                 mode='markers',
                 hoverinfo='text',
                 marker=dict(
                     color=[],
                     size=15,
                     line=dict(width=0))))
-    test = 0
+                
+    test = 0    
+    #make a matrix with figure data per year
+    testlistx = [[] for _ in range(len(years))] 
+    testlisty = [[] for _ in range(len(years))] 
     #add the mails as connections to the trace with the corresponding year   
+
     for edge in G.edges(keys=True):
         year = years.index(G.edges[edge]['year'])
         x0, y0 = G.nodes[edge[0]]['pos']
         x1, y1 = G.nodes[edge[1]]['pos']
-        fig.data[year]['x'] += tuple([x0, x1, None])
-        fig.data[year]['y'] += tuple([y0, y1, None])
+        testlistx[year]+= tuple([x0, x1, None])
+        testlisty[year]+=tuple([y0, y1, None])
 
+    for year in range(len(years)):
+            fig.data[year]['x'] += tuple(testlistx[year])
+            fig.data[year]['y'] += tuple(testlisty[year])
     fig.data[0].visible = True
     #add every person as a node to the trace with the corresponding job title
     for i in range(jobtitlescount):
@@ -232,7 +199,7 @@ def create_network_graph(path):
                     fig.data[inew]['marker']['color']+= tuple([jobcolor[i]])
             for node, adjacencies in enumerate(G.adjacency()):
                 if G.nodes[node+1]['Title'] == jobtitles[i]:
-                    node_info = G.nodes[node+1]['Title']+', ID: '+adjacencies[0].astype(str) + ', mail: ' + G.nodes[node+1]['mail']#DE .astype(str) IS TOEGEVOEGD
+                    node_info =G.nodes[node+1]['mail']#DE .astype(str) IS TOEGEVOEGD
                     fig.data[inew]['text']+=tuple([node_info])
                     node_info =''
 
@@ -261,8 +228,59 @@ def create_network_graph(path):
         sliders=sliders
     )
 
-    return fig
+    app.layout = html.Div([
+            html.Div([
+            dcc.Graph(
+            id='network-graph',
+            figure=fig,
+            #clickData={'points': [{'text': df_enron['fromEmail'].iloc[0]}]}
+            )
+        ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+        html.Div([
+            dcc.Graph(id='sentiment-time-series'),
+            dcc.Graph(id='mail-time-series'),
 
+        ], style={'display': 'inline-block', 'width': '49%'})
+    ])
+    def create_time_series(dff, title, y_axis):
+
+        fig = px.scatter(dff, x='date', y= y_axis)
+
+        fig.update_traces(mode='lines+markers')
+        fig.update_xaxes(showgrid=False)
+
+        fig.update_yaxes(type='linear' )
+
+
+        fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                           xref='paper', yref='paper', showarrow=False, align='left',
+                           bgcolor='rgba(255, 255, 255, 0.5)', text=title)
+
+        fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+        return fig
+    @app.callback(
+        dash.dependencies.Output('sentiment-time-series', 'figure'),
+        [dash.dependencies.Input('network-graph', 'clickData'),])
+
+    def update_sentiment(clickData):
+        email = clickData['points'][0]['text']
+        dff = df_time[df_time['fromEmail'] == email]
+        title = '<b>{}<br><b>{}'.format(email, 'average sentiment over time')
+        y_axis = 'sentiment'
+        return create_time_series(dff, title, y_axis)
+
+    @app.callback(
+        dash.dependencies.Output('mail-time-series', 'figure'),
+        [dash.dependencies.Input('network-graph', 'clickData'),])
+
+    def update_mails(clickData):
+        email = clickData['points'][0]['text']
+        dff = df_time[df_time['fromEmail'] == email]
+        title = '<b>{}<br><b>{}'.format(email, 'amount of sent mails over time')
+        y_axis = 'fromEmailCount'
+        return create_time_series(dff, title, y_axis)
+    return fig
 #This function renders our html page for the visualizations
 def visualization_view(request, *args, **kwargs):
     #search for the correct file in the database by the userid
@@ -273,23 +291,12 @@ def visualization_view(request, *args, **kwargs):
         #set the path to our default database
          path = Path.joinpath(BASE_DIR, "data_set/enron-v1.csv") 
 
-    #call the function to create the line graph and convert it into html
-    line_graph = create_line_graph(path= path).to_html(full_html = False)
+    #call the function to create the network graph and convert it into html
+    network_graph = create_network_graph(path).to_html(full_html = False)
 
     #fill a libary with the extra elements that needs to be loaded into the html
-    context = {"line_graph": line_graph, "network_graph": network_graph}
+    context = {"network_graph": network_graph}
     #render the html file and load in the context
     return render(request, "visualizations.html", context)
     
-#WORK IN PROGRESS
-# def load_line_graph(request):
-#     path = Path.joinpath(BASE_DIR, "data_set/enron-v1.csv")
-#     line_fig = create_line_graph(path= path)
-#     line_graph = line_fig.to_html(full_html=False)
-#     visualization_view(request= request)
 
-# def load_network_graph(request):
-#     path = Path.joinpath(BASE_DIR, "data_set/enron-v1.csv")
-#     network_fig = create_network_graph(path= path)
-#     network_graph = network_fig.to_html(full_html = False) 
-#     visualization_view(request= request)
