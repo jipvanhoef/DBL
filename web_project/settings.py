80a75a2febe98327af8514d4aa982300276c3df6
FriendedBreak <Thymofieten@gmail.com>
2021-06-09 14:41:21 +0200

    added fix to address crash of host

--- web_project/settings.py
+++ web_project/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'static_root')
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR,'data_set')
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
