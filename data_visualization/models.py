2cd1363e81269fcf9854ecaf7fb6d4566794ab6c
FriendedBreak <Thymofieten@gmail.com>
2021-06-11 10:32:51 +0200

    fixed file upload bug

--- data_visualization/models.py
+++ data_visualization/models.py
@@ -10,8 +10,7 @@ user_id = str(uuid.uuid1())
 # Create your models here.
 def file_name(instance, filename):
     #create the path
-    path = Path.joinpath(BASE_DIR,'data_set')
-    path = Path.joinpath(path, user_id)
+    path = Path.joinpath('data_set', user_id)
     path = Path.joinpath(path,filename)
     #return the temporary path 
     return path
@@ -19,7 +18,7 @@ def file_name(instance, filename):
 class Data_set(models.Model):
     #Add a File field to upload the files
     #and set the upload location to the return of the file_name function where the arguments are supplied by django itself
-    file = models.FileField(max_length=255, upload_to= 'data_set')
+    file = models.FileField(max_length=255, upload_to= file_name)
     #create for each user a unique id to reqonize wich user is visualizing which data set
     user_id = models.CharField(default= user_id, max_length=255)
     #save the time when the user summited the data so that it can be determined when the data should be errased
