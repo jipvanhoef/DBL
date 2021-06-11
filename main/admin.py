19074fe5bbe5739e81608ecc1ba911a946e5c813
FriendedBreak <Thymofieten@gmail.com>
2021-06-11 10:12:26 +0200

    added models

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from data_visualization.models import Data_set
-from .models import User
+from main.models import User
 # Register your models here.
 
 admin.site.register(Data_set)
