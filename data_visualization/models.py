d02c562009c7dadacfd707b6d0d64b19cb4d1144
FriendedBreak <Thymofieten@gmail.com>
2021-06-11 10:28:09 +0200

    fixed issue

--- data_visualization/models.py
+++ data_visualization/models.py
@@ -3,6 +3,7 @@ from web_project.settings import BASE_DIR
 from pathlib import Path
 import uuid  
 import datetime
+from django.core.files import File
 # Create your models here.
 
 user_id = str(uuid.uuid1())
@@ -18,7 +19,7 @@ def file_name(instance, filename):
 class Data_set(models.Model):
     #Add a File field to upload the files
     #and set the upload location to the return of the file_name function where the arguments are supplied by django itself
-    file = models.FileField(max_length=255, upload_to= file_name)
+    file = models.FileField(max_length=255, upload_to= 'data_set')
     #create for each user a unique id to reqonize wich user is visualizing which data set
     user_id = models.CharField(default= user_id, max_length=255)
     #save the time when the user summited the data so that it can be determined when the data should be errased
