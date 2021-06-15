8143aa0896d38572ddb4441d2fc9c94daae4db12
FriendedBreak <Thymofieten@gmail.com>
2021-06-15 12:24:54 +0200

    fixed issue of users seeing eachothers data

--- data_visualization/models.py
+++ data_visualization/models.py
@@ -5,12 +5,12 @@ import uuid
 import datetime
 # Create your models here.
 
-user_id = str(uuid.uuid1())
+user_id = uuid.uuid1()
 # Create your models here.
 def file_name(instance, filename):
     #create the path
     parent = Path('data_set')
-    path = Path.joinpath(parent,user_id)
+    path = Path.joinpath(parent,str(user_id))
     path = Path.joinpath(path,filename)
     #return the temporary path 
     return path
@@ -20,7 +20,7 @@ class Data_set(models.Model):
     #and set the upload location to the return of the file_name function where the arguments are supplied by django itself
     file = models.FileField(max_length=255, upload_to= file_name)
     #create for each user a unique id to reqonize wich user is visualizing which data set
-    user_id = models.CharField(default= user_id, max_length=255)
+    user_id = models.UUIDField(default= user_id)
     #save the time when the user summited the data so that it can be determined when the data should be errased
     time = models.DateTimeField(default= datetime.datetime.now().isoformat())
 
--- data_visualization/views.py
+++ data_visualization/views.py
@@ -2,10 +2,17 @@ from django.http import request
 from django.shortcuts import render
 from data_visualization import graph
 from django_plotly_dash.dash_wrapper import DjangoDash
+from main.models import User
+from .models import user_id
 
 
 #This function renders our html page for the visualizations
 def visualization_view(request, *args, **kwargs):
+    graph.build_graph()
+    try:
+        User.objects.get(user_id = user_id)
+    except:
+        User.objects.create(user_id = user_id)
     #render the html file and load in the context
     return render(request, "visualizations.html", {})
     
