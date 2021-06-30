e5f209adb90a2373efbd3e34cf4d416c4117d890
jipvanhoef <jipvanhoef@gmail.com>
2021-06-30 23:54:03 +0200

    small changes to titles of right graphs

--- data_visualization/graph.py
+++ data_visualization/graph.py
@@ -329,9 +329,9 @@ def build_graph():
         dff = df_time_from[(df_time_from['fromEmail'] == email) & (df_time_from['year']==year)]
         dfftot = df_sent[df_sent['year']==year]
         if dff.empty:
-            title = '<b>{}<br><b>{}'.format(email, 'No outgoing mails available for this year!')
+            title = '<b>{}<br>{}{}'.format('No outgoing mails available for this year!', 'Viewing: ', email)
         else:
-            title = '<b>{}<br><b>{}<b>{}'.format(email, 'average sentiment of outgoing mails over time in the year ', year)
+            title = '<b>{}<b>{}<br></b>{}{}'.format('average sentiment of outgoing mails over time in the year ', year, 'Viewing: ', email)
         y_axis = 'sentiment'
         min_date = dff['date'].min()
         max_date = dff['date'].max()
@@ -350,9 +350,9 @@ def build_graph():
         dffto = df_time_to[(df_time_to['toEmail'] == email) & (df_time_to['year']==year)]
         dfffrom = df_time_from[(df_time_from['fromEmail'] == email) & (df_time_from['year']==year)]
         if dfffrom.empty and dffto.empty:
-            title = '<b>{}<br><b>{}'.format(email, 'No in or outgoing mails available for this year!')
+            title = '<b>{}<br>{}{}'.format('No in or outgoing mails available for this year!', 'Viewing: ', email)
         else:
-            title = '<b>{}<br><b>{}<b>{}'.format(email, 'amount of sent and received mails over time in the year ', year)
+            title = '<b>{}<b>{}<br></b>{}{}'.format('Amount of sent and received mails over time in the year ', year, 'Viewing: ', email)
         y_axis1 = 'toEmailCount'
         y_axis2 = 'fromEmailCount'
         return create_time_series(dffto, dfffrom,title, y_axis1, y_axis2)
